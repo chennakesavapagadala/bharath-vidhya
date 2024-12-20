a41ff7770296aa5366a1fca0584390c00234232a
chennakesavapagadala <153279690+chennakesavapagadala@users.noreply.github.com>
2024-12-20 12:05:41 +0530

    Update settings.py

--- CollegeInfo/settings.py
+++ CollegeInfo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(e0$y1mf(q_f)b#ou*sqfa6n3x#*&!ul9w0wf52=y309lv0)dd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
